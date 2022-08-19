8c76d07c3af2b146126b6c0350b1167d069e9b23
yonghwan <sos26276@naver.com>
2022-08-19 11:01:36 +0900

    updates api models, routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,38 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 const App = () => {
-  return <ProductList />
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/products/:category">
+          <ProductList/>
+        </Route>
+        <Route path="/products/:id">
+          <Product/>
+        </Route>
+        <Route path="/cart">
+          <Cart/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/products/:id">
+          <Product/>
+        </Route>
+      </Switch>
+    </Router>
+    )
 };
 
 export default App;
